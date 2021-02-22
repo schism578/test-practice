dd0d09988cae71c0120b5cf08ceb7d7ff6ce3442
Adrian Marquis <adrian.marquis78@gmail.com>
2021-02-21 20:55:27 -0500

    snapshot test added to Messages component

--- src/messages/messages.test.js
+++ src/messages/messages.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer'
 import Messages from './messages';
 
 it('renders without crashing', () => {
@@ -7,3 +8,10 @@ it('renders without crashing', () => {
     ReactDOM.render(<Messages />, div);
     ReactDOM.unmountComponentAtNode(div);
 })
+
+it('renders the UI as expected', () => {
+    const tree = renderer
+        .create(<Messages name='Messages' unread={4} />)
+        .toJSON();
+        expect(tree).toMatchSnapshot();
+});
