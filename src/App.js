7a4b7a6f64c903e793625cfa8d53cf1db3b374fb
Adrian Marquis <adrian.marquis78@gmail.com>
2021-02-21 20:47:22 -0500

    smoke test for child component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import Messages from './messages/messages';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1>APP NAME</h1>
-        <div>Hello!</div>
+        <h1>Messages</h1>
+        <Messages name='Messages' unread={0} />
+        <Messages name='Notifications' unread={10} />
       </div>
     );
   }
---
+++ src/messages/messages.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './messages.css';
+
+export default function Messages(props) {
+    const badge = props.unread ? 
+        <div className='unread_count'>{props.unread}</div> :
+        null;
+        
+        return (
+            <div className='messages'>
+                {props.name}
+                {badge}
+            </div>
+        );
+}
